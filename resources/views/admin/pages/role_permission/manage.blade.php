709c70d788ddd553515fc901db67e120809edecf
Gulácsi András <guland@protonmail.com>
2023-12-25 02:56:50 +0100

    Fix flashing (briefly visible then disappears) of the second tab's content on page load with x-cloak (role-permissions page)

--- resources/views/admin/pages/role_permission/manage.blade.php
+++ resources/views/admin/pages/role_permission/manage.blade.php
@@ -53,9 +53,9 @@
 
                     <!-- Create role -->
                     <livewire:admin.role.create title="{{ __('New role') }}"
-                                          :permissions="$permissions"
-                                          :hasSmallButton="false"
-                                          :modalId="'m-create-role'"
+                                                :permissions="$permissions"
+                                                :hasSmallButton="false"
+                                                :modalId="'m-create-role'"
                     >
                     </livewire:admin.role.create>
 
@@ -87,18 +87,18 @@
                                     <div class="flex flex-row">
                                         <!-- Delete role -->
                                         <livewire:admin.role.delete title="{{ __('Delete role') }}"
-                                                              :role="$role"
-                                                              :hasSmallButton="false"
-                                                              :modalId="'m-delete-role-' . $role->id"
+                                                                    :role="$role"
+                                                                    :hasSmallButton="false"
+                                                                    :modalId="'m-delete-role-' . $role->id"
                                         >
                                         </livewire:admin.role.delete>
 
                                         <!-- Update role -->
                                         <livewire:admin.role.edit title="{{ __('Edit role') }}"
-                                                            :role="$role"
-                                                            :permissions="$permissions"
-                                                            :hasSmallButton="false"
-                                                            :modalId="'m-edit-role-' . $role->id"
+                                                                  :role="$role"
+                                                                  :permissions="$permissions"
+                                                                  :hasSmallButton="false"
+                                                                  :modalId="'m-edit-role-' . $role->id"
                                         >
                                         </livewire:admin.role.edit>
 
@@ -110,71 +110,74 @@
                         @endforeach
                         </tbody>
                     </table>
+
                 </div>
 
                 <div id="Permissions" class="box tabs animate-opacity">
 
+                    <div x-data="{}" x-cloak>
+                        <h1 class="h3">{{ __('Manage permissions') }}</h1>
 
-                    <h1 class="h3">{{ __('Manage permissions') }}</h1>
+                        <!-- Create role -->
+                        <livewire:admin.permission.create title="{{ __('New permission') }}" :roles="$roles"
+                                                          :hasSmallButton="false"
+                                                          :modalId="'m-create-permission'">
+                        </livewire:admin.permission.create>
 
-                    <!-- Create role -->
-                    <livewire:admin.permission.create title="{{ __('New permission') }}" :roles="$roles"
-                                                :hasSmallButton="false"
-                                                :modalId="'m-create-permission'">
-                    </livewire:admin.permission.create>
-
-                    <table>
-                        <thead>
-                        <tr class="fs-14">
-                            <th>{{ __('Permission') }}</th>
-                            <th>{{ __('Slug') }}</th>
-                            <th>{{ __('Roles') }}</th>
-                            <th>{{ __('Actions') }}</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($permissions as $permission)
-                            <tr>
-                                <td><b>{{ $permission->name }}</b></td>
-                                <td class="italic">{{ $permission->slug }}</td>
-                                <td class="fs-12">
-                                    @if($permission->roles->count() > 0)
-                                        @foreach($permission->roles as $permissionRole)
-                                            <span
-                                                class="badge gray-60 margin-bottom-0-5">{{ $permissionRole->name }}</span>
-                                        @endforeach
-                                    @else
-                                        <p class="text-gray-60">{{__('No associated roles.')}}</p>
-                                    @endif
-                                </td>
-
-                                <td>
-                                    <div class="flex flex-row">
-                                        <!-- Delete role -->
-                                        <livewire:admin.permission.delete title="{{ __('Delete permission') }}"
-                                                                    :permission="$permission"
-                                                                    :hasSmallButton="false"
-                                                                    :modalId="'m-delete-permission-' . $permission->id"
-                                        >
-                                        </livewire:admin.permission.delete>
-
-                                        <!-- Update role -->
-                                        <livewire:admin.permission.edit title="{{ __('Edit permission') }}"
-                                                                  :permission="$permission"
-                                                                  :roles="$roles"
-                                                                  :hasSmallButton="false"
-                                                                  :modalId="'m-edit-permission-' . $permission->id"
-                                        >
-                                        </livewire:admin.permission.edit>
-
-                                    </div>
-
-                                </td>
-
+                        <table>
+                            <thead>
+                            <tr class="fs-14">
+                                <th>{{ __('Permission') }}</th>
+                                <th>{{ __('Slug') }}</th>
+                                <th>{{ __('Roles') }}</th>
+                                <th>{{ __('Actions') }}</th>
                             </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                            @foreach($permissions as $permission)
+                                <tr>
+                                    <td><b>{{ $permission->name }}</b></td>
+                                    <td class="italic">{{ $permission->slug }}</td>
+                                    <td class="fs-12">
+                                        @if($permission->roles->count() > 0)
+                                            @foreach($permission->roles as $permissionRole)
+                                                <span
+                                                    class="badge gray-60 margin-bottom-0-5">{{ $permissionRole->name }}</span>
+                                            @endforeach
+                                        @else
+                                            <p class="text-gray-60">{{__('No associated roles.')}}</p>
+                                        @endif
+                                    </td>
+
+                                    <td>
+                                        <div class="flex flex-row">
+                                            <!-- Delete role -->
+                                            <livewire:admin.permission.delete title="{{ __('Delete permission') }}"
+                                                                              :permission="$permission"
+                                                                              :hasSmallButton="false"
+                                                                              :modalId="'m-delete-permission-' . $permission->id"
+                                            >
+                                            </livewire:admin.permission.delete>
+
+                                            <!-- Update role -->
+                                            <livewire:admin.permission.edit title="{{ __('Edit permission') }}"
+                                                                            :permission="$permission"
+                                                                            :roles="$roles"
+                                                                            :hasSmallButton="false"
+                                                                            :modalId="'m-edit-permission-' . $permission->id"
+                                            >
+                                            </livewire:admin.permission.edit>
+
+                                        </div>
+
+                                    </td>
+
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+
+                    </div>
 
 
                 </div>
