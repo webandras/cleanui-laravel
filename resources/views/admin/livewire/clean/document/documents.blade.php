b1c69785811915d1ffe0d510fb0079b9e2716c65
Gulácsi András <guland@protonmail.com>
2024-03-18 02:08:17 +0100

    Fix styling for manage documents page

--- resources/views/admin/livewire/clean/document/documents.blade.php
+++ resources/views/admin/livewire/clean/document/documents.blade.php
@@ -1,11 +1,11 @@
 <div>
-    <ul wire:sortable="updateOrder" class="no-bullets padding-left-right-0-5">
+    <ul wire:sortable="updateOrder" class="document__list no-bullets padding-left-right-0-5">
         @foreach ($documents as $document)
             <li wire:sortable.item="{{ $document->id }}" wire:key="document-{{ $document->id }}"
-                class="border round border-light margin-bottom-1 padding-left-right-1">
-                <div class="flex flex-row" style="justify-content: space-between;">
-                    <div wire:sortable.handle class="flex-row" style="display:flex;align-items: center;cursor:move">
-                        <div style="display:flex;align-items: center;"><i
+                class="document__list__item border round border-light margin-bottom-1 padding-left-right-1">
+                <div class="flex flex-row document__list__item__container">
+                    <div wire:sortable.handle class="document__list__item__container__content flex-row">
+                        <div class="document__list__item__container__content__grip"><i
                                 class="fa-solid fa-grip-vertical text-gray-40 margin-right-0-5"></i></div>
                         <div class="flex flex-column">
                             <h4 class="h5">{{ $document->title }} <span
@@ -15,7 +15,7 @@
 
                         </div>
                     </div>
-                    <div class="button-group padding-top-bottom-1" style="align-items: center;">
+                    <div class="button-group padding-top-bottom-1">
                         <!-- View document -->
                         <a href="{{ route('document.show', $document->slug) }}"
                            target="_blank"
