527b950bb36e01603a34cd31a576d1f7a0e4b98c
Gulácsi András <guland@protonmail.com>
2024-05-11 00:06:28 +0200

    Update UserSeeder.php with faker-generated names and emails, update passwords

--- database/seeders/Auth/Clean/UserSeeder.php
+++ database/seeders/Auth/Clean/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders\Auth\Clean;
 
 use App\Models\Clean\Role;
 use App\Models\Clean\User;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -15,6 +16,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Factory::create();
 
         $superAdmin = Role::where('slug', 'super-administrator')->firstOrFail();
         $admin = Role::where('slug', 'administrator')->firstOrFail();
@@ -23,22 +25,22 @@ class UserSeeder extends Seeder
         $user1 = new User();
         $user1->name = 'Gulácsi András';
         $user1->email = 'gulandras90@gmail.com';
-        $user1->password = bcrypt('FSrztfvr5gii2');
+        $user1->password = bcrypt('g9QFR&bvsBW;ZrT%1)Fd');
         $user1->enable_2fa = 1;
         $user1->role()->associate($superAdmin);
         $user1->save();
 
         $user2 = new User();
-        $user2->name = 'John Doe';
-        $user2->email = 'john@doe.com';
-        $user2->password = bcrypt('password');
+        $user2->name = $faker->name();
+        $user2->email = $faker->safeEmail();
+        $user2->password = bcrypt('bHYm2YnR^0Dpmxb17k^2');
         $user2->save();
         $user2->role()->associate($admin);
 
         $user2 = new User();
-        $user2->name = 'Finn Gabika';
-        $user2->email = 'finn@gabika.com';
-        $user2->password = bcrypt('password');
+        $user2->name = $faker->name();
+        $user2->email = $faker->safeEmail();
+        $user2->password = bcrypt('3dSRqkUBafX(w8W7Dut(');
         $user2->save();
         $user2->role()->associate($customer);
 
