1b799a6515fc77da0dfb26f1a801c6f4ae40b05a
Gulácsi András <guland@protonmail.com>
2024-05-26 17:43:17 +0200

    Fix errors on blog view when there are no posts or categories in the database tables

--- resources/views/public/pages/blog/index.blade.php
+++ resources/views/public/pages/blog/index.blade.php
@@ -14,75 +14,81 @@
 
         <h1 class="text-left margin-top-0 margin-bottom-0-5">{{ __('Newest articles') }}</h1>
 
-        <section class="intro">
-            <div class="left-panel-1">
-                <article class="card margin-bottom-0">
-                    <a href="{{ route('blog.show', $newestPosts[0]->slug) }}" class="no-underline">
-                        <div class="relative cover-image-container">
-                            <img class="round-top" src="{{ $newestPosts[0]->cover_image_url }}"
-                                 alt="{{ $newestPosts[0]->title }}">
-                        </div>
-                    </a>
-                    <div class="padding-left-right-1">
-                        <div
-                            class="date fs-14">{{ Carbon\Carbon::parse($newestPosts[0]->created_at)->translatedFormat($dtFormat) }}</div>
-                        <h2 class="margin-top-0 h4 text-white fs-16 margin-bottom-45">
-                            <a href="{{ route('blog.show', $newestPosts[0]->slug) }}" class="no-underline">
-                                {{ $newestPosts[0]->title }}
-                            </a>
-                        </h2>
-                        <a href="{{ route('blog.show', $newestPosts[0]->slug) }}" class="button read-more margin-top-0">
-                            {{ __('Read more') }}<i class="fa-solid fa-angles-right margin-left-0-5"></i>
-                        </a>
-                    </div>
-                </article>
-
-            </div>
-            <div class="right-panel-2">
-
-                @for($i = 1; $i <= 2; $i++)
-                    <article>
-                        <a href="{{ route('blog.show', $newestPosts[$i]->slug) }}" class="no-underline">
-                            <div class="relative cover-image-container card">
-                                <img class="round"
-                                     src="{{ asset($newestPosts[$i]->cover_image_url) ?? asset('/images/placeholder.png') }}"
-                                     alt="{{ $newestPosts[0]->title }}"
-                                >
+        @if ($newestPosts->count() === 0)
+            <p>No posts in the database. Run the seeder.</p>
+        @else
+            <section class="intro">
+                <div class="left-panel-1">
+                    <article class="card margin-bottom-0">
+                        <a href="{{ route('blog.show', $newestPosts[0]->slug) }}" class="no-underline">
+                            <div class="relative cover-image-container">
+                                <img class="round-top" src="{{ $newestPosts[0]->cover_image_url }}"
+                                     alt="{{ $newestPosts[0]->title }}">
                             </div>
                         </a>
-                        <div class="padding-top-bottom-0-5">
-                            <h2 class="margin-top-bottom-0 h4 fs-18">
-                                <a href="{{ route('blog.show', $newestPosts[$i]->slug) }}" class="no-underline">
-                                    {{ $newestPosts[$i]->title }}
+                        <div class="padding-left-right-1">
+                            <div
+                                class="date fs-14">{{ Carbon\Carbon::parse($newestPosts[0]->created_at)->translatedFormat($dtFormat) }}</div>
+                            <h2 class="margin-top-0 h4 text-white fs-16 margin-bottom-45">
+                                <a href="{{ route('blog.show', $newestPosts[0]->slug) }}" class="no-underline">
+                                    {{ $newestPosts[0]->title }}
                                 </a>
                             </h2>
+                            <a href="{{ route('blog.show', $newestPosts[0]->slug) }}"
+                               class="button read-more margin-top-0">
+                                {{ __('Read more') }}<i class="fa-solid fa-angles-right margin-left-0-5"></i>
+                            </a>
                         </div>
-
                     </article>
-                @endfor
-            </div>
-            <div class="side-panel-4">
 
-                <h2>{{ __('Additional articles') }}</h2>
+                </div>
+                <div class="right-panel-2">
 
-                <ul class="no-bullets">
-                    @for($i = 3; $i < $newestPosts->count(); $i++)
-                        <li>
-                            <article>
-                                <div
-                                    class="date fs-14">{{ Carbon\Carbon::parse($newestPosts[$i]->created_at)->translatedFormat($dtFormat) }}</div>
-                                <h3 class="fs-16">
+                    @for($i = 1; $i <= 2; $i++)
+                        <article>
+                            <a href="{{ route('blog.show', $newestPosts[$i]->slug) }}" class="no-underline">
+                                <div class="relative cover-image-container card">
+                                    <img class="round"
+                                         src="{{ asset($newestPosts[$i]->cover_image_url) ?? asset('/images/placeholder.png') }}"
+                                         alt="{{ $newestPosts[0]->title }}"
+                                    >
+                                </div>
+                            </a>
+                            <div class="padding-top-bottom-0-5">
+                                <h2 class="margin-top-bottom-0 h4 fs-18">
                                     <a href="{{ route('blog.show', $newestPosts[$i]->slug) }}" class="no-underline">
-                                        {{$newestPosts[$i]->title }}</a>
-                                </h3>
-                            </article>
-                        </li>
+                                        {{ $newestPosts[$i]->title }}
+                                    </a>
+                                </h2>
+                            </div>
+
+                        </article>
                     @endfor
-                </ul>
+                </div>
+                <div class="side-panel-4">
 
-            </div>
+                    <h2>{{ __('Additional articles') }}</h2>
 
-        </section>
+                    <ul class="no-bullets">
+                        @for($i = 3; $i < $newestPosts->count(); $i++)
+                            <li>
+                                <article>
+                                    <div
+                                        class="date fs-14">{{ Carbon\Carbon::parse($newestPosts[$i]->created_at)->translatedFormat($dtFormat) }}</div>
+                                    <h3 class="fs-16">
+                                        <a href="{{ route('blog.show', $newestPosts[$i]->slug) }}" class="no-underline">
+                                            {{$newestPosts[$i]->title }}</a>
+                                    </h3>
+                                </article>
+                            </li>
+                        @endfor
+                    </ul>
+
+                </div>
+
+            </section>
+
+        @endif
 
         <hr>
 
@@ -94,17 +100,21 @@
                 <h2>{{ __('Most popular categories') }}</h2>
                 <div class="separator"></div>
             </div>
-
+            @if ($categories->count() > 0)
             <nav class="categories-container">
                 @foreach($categories as $cat)
 
                     <a href="{{ route('blog.category', $cat->slug) }}">{{ $cat->name }}</a>
                 @endforeach
             </nav>
+            @else
+            <p>No categories in the database. Run the seeder.</p>
+            @endif
         </section>
 
         <hr>
 
+        @if ($highlightedPosts->count() > 0)
         <section class="highlighted-posts-container">
             <div class="featured">
                 @foreach($highlightedPosts as $highlightedPost)
@@ -157,6 +167,9 @@
             </div>
 
         </section>
+        @else
+            <p>No highlighted posts in the database. Run the seeder.</p>
+        @endif
 
 
     </div>
