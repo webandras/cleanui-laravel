f045523022c9ef4c3a1562c08ab9adec21000fbb
Gulácsi András <guland@protonmail.com>
2024-09-23 23:57:25 +0200

    Register custom console command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,11 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        \Modules\Clean\Console\Commands\RefreshDatabase::class,
+    ];
+
+
     /**
      * Define the application's command schedule.
      */
