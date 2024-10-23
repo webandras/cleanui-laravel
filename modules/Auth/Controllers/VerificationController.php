76b174d4d378a31e2ea6655bf8b401ed6a26f4ea
Gulácsi András <guland@protonmail.com>
2024-10-24 00:11:11 +0200

    Fix incorrect view path

--- modules/Auth/Controllers/VerificationController.php
+++ modules/Auth/Controllers/VerificationController.php
@@ -5,6 +5,7 @@ namespace Modules\Auth\Controllers;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\VerifiesEmails;
+use Illuminate\Http\Request;
 
 class VerificationController extends Controller
 {
@@ -40,5 +41,12 @@ class VerificationController extends Controller
         $this->middleware('auth');
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
+    }
+
+    public function show(Request $request)
+    {
+        return $request->user()->hasVerifiedEmail()
+            ? redirect($this->redirectPath())
+            : view('auth::public.verify');
     }
 }
